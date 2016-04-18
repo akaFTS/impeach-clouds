865669b3c041a2eb4878750aa2e0556d7ffe8ec8
akaFTS <gustavo84171@hotmail.com>
2016-04-18 15:50:18 -0300

    Melhor gerenciamento de quebras de linha

--- bin/filtrafalas.sh
+++ bin/filtrafalas.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+#Remover todos os newlines, exceto ao trocar de personagem
+FALAS=$(cat $1 | tr '\n' ' ' | sed 's/O SR./\'$'\nO SR./g' | sed 's/A SRA./\'$'\nA SRA./g')
+
 #Remover falas dos capangas do Cunha
-FALAS=$(cat $1 | grep -Ev "PRESIDENTE|BETO MANSUR|FELIPE BORNIER|ALEX CANZIANI")
+FALAS=$(echo "${FALAS}" | grep -Ev "PRESIDENTE|BETO MANSUR|FELIPE BORNIER|ALEX CANZIANI")
 
 #Remover partido e estado, e tambem o "O SR." antes do nome
 FALAS=$(echo "${FALAS}" | sed -e 's/([^()]*)//g' | sed -e 's/O SR.//g' | awk '$1=$1')
